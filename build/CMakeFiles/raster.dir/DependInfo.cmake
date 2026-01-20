4a76b72c70f588848246ec703a6025d9bd9afbc6
EllaHanson <ellahanson.126@gmail.com>
2026-01-19 18:02:01 -0800

    initial

---
+++ build/CMakeFiles/raster.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/ella/Desktop/Winter 26/Computer Graphics/Week 2/HW 1/P1RasterBase2020/src/Image.cpp" "CMakeFiles/raster.dir/src/Image.cpp.o" "gcc" "CMakeFiles/raster.dir/src/Image.cpp.o.d"
+  "/Users/ella/Desktop/Winter 26/Computer Graphics/Week 2/HW 1/P1RasterBase2020/src/main.cpp" "CMakeFiles/raster.dir/src/main.cpp.o" "gcc" "CMakeFiles/raster.dir/src/main.cpp.o.d"
+  "/Users/ella/Desktop/Winter 26/Computer Graphics/Week 2/HW 1/P1RasterBase2020/src/tiny_obj_loader.cpp" "CMakeFiles/raster.dir/src/tiny_obj_loader.cpp.o" "gcc" "CMakeFiles/raster.dir/src/tiny_obj_loader.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
